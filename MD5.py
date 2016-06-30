46286f8e9fbde669707f7f65cfcef282798606db
rdczowh <worldisok@live.com>
2016-06-29 21:25:59 -0700

    add few space line as test for commit

--- MD5.py
+++ MD5.py
@@ -4,13 +4,16 @@ db = {}
 def get_md5(str):
     md5 = hashlib.md5()
     md5.update(str.encode('utf-8'))
+	
     return md5.hexdigest()
 
 def register(username, password):
     db[username] = get_md5(password + username + 'the-Salt')
+	
 def login(username, password):
     if(db.get(username)==get_md5(password + username + 'the-Salt')):
         print("Hello,%s,Login sucessful!" % username)
+		
     else:
         print("username or password Error!")
 
